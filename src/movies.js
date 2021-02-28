928b1b227602fc50c15f0643219b1dfc818fed8c
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-02-28 14:29:57 +0100

    Iteration 3 (manque si les films n'ont pas de rate , iteration 4

--- src/movies.js
+++ src/movies.js
@@ -19,45 +19,53 @@ function howManyMovies(someArr) {
         return el.director === "Steven Spielberg" && el.genre.includes("Drama");
 
     });
-
-    console.log(steven.length);
-
     return steven.length;
-
 }
-
-howManyMovies(movies);
-
-
-
-
-
-
-// On parcourt l'array et on filtre de steven spielberg
-// On filtre dans les films de Steven Spielberg, on filtre DRAMA
-// Retourne uniquement les films drama de Steven Spielberg 
-
-
-
-
-
-
-
-
-// Si lors de la boucle, steven spielbierg -> 
-// If steven spielbierg 
-
-
-
-
 
 
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(someArr){
+    // Si il n'y a aucun film, la fonction retourne 0
+    if (someArr.length === 0) {
+        return 0;
+    }
+    // on réduit la rate de tout les films à un seul nombre
+    let rateCumul = someArr.reduce(function (acc, value){
+        return acc + value.rate;
+    }, 0);
+    // on calcul ensuite la moyenne de cette rate en la divisant par le nombre de films
+    let average = (rateCumul / (someArr.length)).toFixed(2)
+    return Number(average)
+}
+
+
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(someArr){
+    // on filtre d'abord tout les films qui ont pour genre Drama
+    let dramaMovies = someArr.filter(function(el) {
+        return el.genre.includes("Drama");
+    });
+    // si il n'y a aucun film avec le genre Drama, la fonction retourne 0
+    if (dramaMovies.length === 0){
+        return 0;
+    }
+    // on réduit la rate de tout les film du genre Drama à un seul nombre
+    let rateCumul = dramaMovies.reduce(function (acc, value) {
+        return acc + value.rate;
+    }, 0);
+    // on calcul ensuite la moyenne de cette rate en la divisant par le nombre de films du genre Drama
+    let average = (rateCumul / (dramaMovies.length)).toFixed(2);
+    return Number(average);
+
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
